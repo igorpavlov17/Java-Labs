92e0356b7eb8f360eab116dd89e129b369be5114
igorpavlov17 <iterx73@yandex.ru>
2022-09-11 18:10:15 +0300

    lab1

---
+++ ru/rsatu/lab1/Class1.java
@@ -0,0 +1,7 @@
+public class Class1{
+    public static void main(String args[]){
+        Class2.method1();
+        Class2 class2 = new Class2();
+        class2.method2();
+    }
+}
---
+++ ru/rsatu/lab1/Class2.java
@@ -0,0 +1,9 @@
+public class Class2{
+    public static void method1(){
+        System.out.println("method1");
+    }
+
+    public void method2(){
+        System.out.println("method2");
+    }
+}
